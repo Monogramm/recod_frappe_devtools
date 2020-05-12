45f1f4db2d18fd292bae6ddda468d0b2bad40f28
Emil <amionv99@gmail.com>
2020-05-12 17:01:07 +0300

    :white_check_mark: Add tests for uml generation

--- recod_frappe_devtools/tests/test_commands.py
+++ recod_frappe_devtools/tests/test_commands.py
@@ -12,9 +12,12 @@ from recod_frappe_devtools.commands import _build_docs_once
 
 class TestCommands(unittest.TestCase):
     def setUp(self):
-
         self.context = {'sites': frappe.utils.get_sites(), 'force': False, 'verbose': False, 'profile': False}
-
-    def test_build_app_docs(self):
         _build_docs_once(self.context['sites'][0], "recod_frappe_devtools", "current", "recod_frappe_devtools", "local")
+
+    def test_build_app_docs_one_checker(self):
         self.assertTrue(frappe.get_app_path("recod_frappe_devtools", 'www'))
+
+    def test_add_uml_file_exists(self):
+        self.assertTrue(frappe.get_app_path("recod_frappe_devtools", 'www', 'docs', 'recod_frappe_devtools', 'assets',
+                                            'generated_uml.pdf'))
